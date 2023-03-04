23645366c9c04b6590dc0b7782bbd414e9a9c663
S Vega P <71044707+riffosaurus@users.noreply.github.com>
2023-03-04 15:30:24 -0300

    botones pokemon card

--- src/pages/CardR.jsx
+++ src/pages/CardR.jsx
@@ -1,5 +1,5 @@
 import { Card, Container, Row, Col } from 'react-bootstrap'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import React from 'react'
 import { useState, useEffect } from 'react';
 
@@ -7,17 +7,37 @@ import { useState, useEffect } from 'react';
 
 const CardR = () => {
 
-    //useEffect para hacer la busqueda de pokemon
-    useEffect(() => {
-        getPokemon();
-        getPokemonSpecies();
-    }, []);
+  
 
     const urlPokeapi = "https://pokeapi.co/api/v2/pokemon/";
     const urlPokeSpecies = "https://pokeapi.co/api/v2/pokemon-species/";
     const { id } = useParams();
     const [datosPokemon, setDatosPokemon] = useState("");
     const [datosPokemonSpecies, setDatosPokemonSpecies] = useState("");
+    const navigate = useNavigate();
+
+      //useEffect para hacer la busqueda de pokemon
+      useEffect(() => {
+        getPokemon();
+        getPokemonSpecies();
+    }, [id]
+    );
+ 
+
+//funcion para navegar al pokemon anterior de la lista
+const irAPokemonAnterior = () => {
+    if (id > 1) {
+        navigate(`/pokemones/${parseInt(id) - 1}`);
+    }
+}
+
+//funcion para navegar al pokemon siguiente de la lista
+const irAPokemonSiguiente = () => {
+    if (id < 151) {
+        navigate(`/pokemones/${parseInt(id) + 1}`);
+    }
+}
+
 
 
 
@@ -53,20 +73,31 @@ const CardR = () => {
                 <Container className=''>
                     <Row className=''>
                         <Col className=''>
-                <Card style={{ width: '80rem' }} className=' align-items-center w-100 h-auto'>
-                    <Card.Img variant="top" src={datosPokemon.sprites.other["official-artwork"].front_default} className='w-75 h-auto'/>
+                <Card  className=' align-items-center w-auto h-100'>
+                    <Card.Img variant="top" src={datosPokemon.sprites.other["official-artwork"].front_default} className='w-auto h-100'/>
                     <Card.Body>
                         <Card.Title><h1>{datosPokemon.forms[0].name.charAt(0).toUpperCase()+datosPokemon.forms[0].name.slice(1)}</h1></Card.Title>
                         <Card.Text>
-                            
+                            <br></br>
                             {datosPokemonSpecies.flavor_text_entries[26].flavor_text}
-                            
+                            <br></br>
                         </Card.Text>
+                        
                     </Card.Body>
                 </Card>
                 </Col>
                 </Row>
                 </Container>
+                <br></br>
+                <Row className=''>
+                    {/* dos botones de bootstrap */}
+                    <Col className=''>
+                        <button className="btn btn-primary" onClick={irAPokemonAnterior}>Anterior</button>
+                    </Col>
+                    <Col className=''>
+                        <button className="btn btn-primary" onClick={irAPokemonSiguiente}>Siguiente</button>
+                    </Col>
+                </Row>
             </div>
         )
     }
